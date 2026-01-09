4b4b07e07ca8048bac49d1c2f7053ce96b634c87
mynxdmine-web <mynxdmine@gmail.com>
2026-01-09 20:03:27 -0300

    Update index.js
    kk

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host:vizzioland.aternos.me
+  host: vizzioland.aternos.me
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username:vizzio 
-  port:37889
+  username: vizzio 
+  port: 37889
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
